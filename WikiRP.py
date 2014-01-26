c69d48ff82b8dda3bb85705b214176e78350c664
Alex Vallorosi <reddog9287@aim.com>
2014-01-26 00:34:57 -0500

    updating error handling

--- WikiRP.py
+++ WikiRP.py
@@ -5,13 +5,23 @@ class WikiRP(object):
 	def __init__(self):
 		self.thesis = raw_input("Enter a thesis statement: ")
 
-	def makeRequest(self):
-		url = "http://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&titles="+self.thesis
+	def makeRequest(self, method):
+		body = "http://en.wikipedia.org/w/api.php?format=json&action=query"
+
+		if method == "search":
+			params = "&list=search&srprop=timestamp&srsearch="+self.thesis
+		else:
+			params = "&prop=revisions&rvprop=content&titles="+self.thesis
+
+		url = body + params
 		response = urllib2.urlopen(url)
 		json = response.read()
 		out = ast.literal_eval(json)
 		# Below qould be useful but idk not right now
 		# print "I'm a %s %s and I taste %s." % (self.color, self.name, self.flavor)
+
+		# Oops! Obviously this below will be different if it's search vs pages req
+		# Make this all functional (ie. create a parse method)
 		pageid = out["query"]["pages"].keys()[0]
 		content = out["query"]["pages"][pageid]["revisions"][0]["*"]
 		print content
@@ -27,5 +37,6 @@ class WikiRP(object):
 
 researchPaper = WikiRP()
 # This all goes in a "main" method
-researchPaper.makeRequest()
-researchPaper.errors()
+researchPaper.makeRequest("pages")
+if researchPaper.errors():
+	researchPaper.makeRequest("search")
